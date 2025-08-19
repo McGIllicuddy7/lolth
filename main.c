25e6399dcf6641d84652c04728fc1237f3b2b0f2
Bridget Hartin Pasco <isaacpasco@icloud.com>
2025-08-18 23:03:06 -0700

    bruh

--- main.c
+++ main.c
@@ -1,57 +1,40 @@
 #include <stdio.h>
-#include "reflect.h"
-typedef struct Test{
-	int x;
-	int y;
-	char * name;
-	struct Test * next;
-}Test;
-void debug_print(Reflection v){
-	printf("{");
-	for(size_t i =0; i<v.data->field_count; i++){
-		RValue f = reflect_get(v, i);
-		const char * fname = f.field->field_name;
-		if(reflect_is_int(f)){
-			printf("%s: %ld", fname,reflect_get_int(f));
-		}else if(reflect_is_uint(f)){
-			printf("%s: %lu", fname,reflect_get_uint(f));
-		}else if(reflect_is_c_str(f)){
-			printf("%s: \"%s\"", fname,reflect_get_c_str(f));
-		}else if(reflect_is_float(f)){
-			printf("%s: %f",fname, reflect_get_float(f));
-		}else if(reflect_is_struct(f)){
-			printf("%s: ",fname);
-			debug_print(reflect_get_struct(f));
-		}else if(reflect_is_ptr(f)){
-			printf("%s: ",fname);
-			Reflection r = reflect_get_ptr(f);
-			if(r.ptr){
-				debug_print(r);
-			}else{
-				printf("null");
-			}
-		}else{
-			printf("ERROR:else");
-		}
-		if(i != v.data->field_count-1){
-			printf(", ");
-		}
+#include "context.h"
+#include <stdlib.h>
+#include <stdatomic.h>
+bool done = false;
+atomic_size_t counter =0;
+extern thread_local size_t current_task;
+void test(void*ptr);
+void test_spawner(void* arg){
+	task_handle_t tasks[100] = {0};
+	for(int i =0; i<10; i++){
+		tasks[i] = task_spawn(test, 0);
 	}
-	printf("}\n");
-}
+/*	for(int i =0; i<10; i++){
 
-make_reflection(Test, reflect_data(Test,x), reflect_data(Test,y), reflect_c_str(Test, name), reflect_pointer(Test, next, Test));
+		task_await(tasks[i]);
+	}*/
+
+}
+void test(void*ptr ){
+	for(int i =0; i<10; i++){
+		counter++;
+		printf("%d\n",i);
+		yield();
+	}		
+}
 int main(){
-	Test test2 = {0};
-	test2.x = 32;
-	test2.y = 16;
-	test2.name = "test2";
-	test2.next =0;
-	Test test = {0};
-	test.x = 0;
-	test.y = 0;
-	test.name = ":3";
-	test.next =&test2;
-	Reflection s = reflect(Test,&test);
-	debug_print(s);
+	lolth_init();
+	task_handle_t tsks[100] = {0};
+	for(int i =0; i<100; i++){
+		tsks[i] = task_spawn(test,0);
+	}
+	for(int i =0; i<100; i++){	
+		printf("context?\n");
+		task_await(tsks[i]);
+	}	
+	printf("%zu, %zu\n",counter, current_task);
+	exit(0);
+
 }
--- makefile
+++ makefile
@@ -1,2 +1,5 @@
-make: main.c
-	gcc main.c -Wall -Wextra -std=c23
+make: main.c context.c  context.s
+	gcc main.c context.c context.s -Wall -Wextra -std=c23 -g3 
+expand: main.c context.c  context.s
+	gcc main.c context.c context.s -Wall -Wextra -std=c23 -g3 -S
+
